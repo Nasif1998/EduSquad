fba69f894025aa6d72d44797461c91e9268d7cb5
hadiuldev <hadiulislamofficial@gmail.com>
2021-05-29 16:21:21 +0600

    add login page1

--- src/Components/Core/Home/Home/Home.js
+++ src/Components/Core/Home/Home/Home.js
@@ -1,21 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import Banner from '../Banner/Banner';
-=======
 import Footer from '../../../Global/Footer/Footer';
 import Banner from '../Banner/Banner';
 import Contact from'../../Contact/Contact';
->>>>>>> 072f834001235c147bfcd5cbbcd94dc5be00c99e
 
 const Home = () => {
     return (
         <>
             <Banner />
-<<<<<<< HEAD
-=======
             <Contact></Contact>
             <Footer></Footer>
->>>>>>> 072f834001235c147bfcd5cbbcd94dc5be00c99e
         </>
     );
 };
