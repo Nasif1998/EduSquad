e8b991dcdb95ae9b2c80deef93f78e9bd4874f65
Nasif123 <nasif.reza@northsouth.edu>
2021-05-29 15:59:39 +0600

    contact form and fake data

--- src/Components/Core/Home/Home/Home.js
+++ src/Components/Core/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../../Global/Footer/Footer';
 import Banner from '../Banner/Banner';
+import Contact from'../../Contact/Contact';
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <Contact></Contact>
             <Footer></Footer>
         </>
     );
