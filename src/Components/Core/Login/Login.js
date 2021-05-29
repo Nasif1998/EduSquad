30ba099e13b760b0f3be2e0d3817258d58cac295
Hadiul Islam <75871974+Hadiul-Islam@users.noreply.github.com>
2021-05-29 16:13:31 +0600

    Hadiul/nav banner (#15)
    * setup nav-banner-loginpage
    
    * design hero section
    
    * add hero section

--- src/Components/Core/Login/Login.js
+++ src/Components/Core/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login page</h1>
-        </div>
+        <section className="hero-login d-flex flex-column justify-content-center">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<h1>New World For Education!</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident reiciendis omnis, mollitia nisi suscipit dolorum praesentium nam magni similique ducimus.
+						</p>
+						<Link to="/login" href="index.html" className="btn1">All Courses</Link>
+					</div>
+				</div>
+			</div>
+		</section>
     );
 };
 
