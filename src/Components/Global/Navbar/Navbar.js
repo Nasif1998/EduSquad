e97c53d1602ce08af7d4427dbefe193c30235877
Hadiul Islam <75871974+Hadiul-Islam@users.noreply.github.com>
2021-05-29 12:30:07 +0600

    Hadiul/navbar add design (#9)
    * setup nav-banner-loginpage
    
    * design hero section
    
    * add hero section

--- src/Components/Global/Navbar/Navbar.js
+++ src/Components/Global/Navbar/Navbar.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link active" aria-current="page" >Login</Link>
-                        </li>
-                    </ul>
+
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">Edu Squad</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link active">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 };
 
