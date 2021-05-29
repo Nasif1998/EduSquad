0b737046e7fa7e521c695af6a7e100b14fa20cf5
hadiuldev <hadiulislamofficial@gmail.com>
2021-05-29 16:50:42 +0600

    add auth

--- src/Components/Core/Login/Login.js
+++ src/Components/Core/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import login from '../../../image/login-banner.jpg'
 import './login.css'
 
 const Login = () => {
@@ -7,13 +8,12 @@ const Login = () => {
         <section className="hero-login d-flex flex-column justify-content-center">
 			<div className="container">
 				<div className="row">
-					<div className="col-md-6">
-						<h1>New World For Education!</h1>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident reiciendis omnis, mollitia nisi suscipit dolorum praesentium nam magni similique ducimus.
-						</p>
-						<Link to="/login" href="index.html" className="btn1">All Courses</Link>
-					</div>
+                <div className="col-md-6 col-sm-6 col-12">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, labore! Ipsa ipsam, at fugiat dolor recusandae, expedita vel alias error perferendis magni eveniet. Soluta quaerat, illo quibusdam ipsam fugit beatae iure. Eius, minima obcaecati inventore consectetur, quisquam ipsum nisi odit exercitationem blanditiis, quas voluptas maiores enim! Laboriosam, numquam. Debitis sapiente non quae totam ea blanditiis, in aperiam fugiat iure nam.</p>
+                </div>
+                <div className="col-md-6 col-sm-6 col-12">
+
+                </div>
 				</div>
 			</div>
 		</section>
