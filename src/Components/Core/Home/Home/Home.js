5f0c83332c3e10d564203cca5e0dea30afcd8f0b
mustaquenadim <mustaquenadim2000@gmail.com>
2021-05-30 10:24:32 +0600

    redux is working

--- src/Components/Core/Home/Home/Home.js
+++ src/Components/Core/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../../Global/Footer/Footer';
+import Courses from '../../Courses/Courses';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <Courses />
             <Footer></Footer>
         </>
     );
